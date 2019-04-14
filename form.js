f341d97b3a3c7a8eeedfe15c452e9ca22196e3f3
Elena Ilchenko <elena-ask@yandex.ru>
2019-04-14 13:50:30 +0300

    make js validation form

---
+++ form.js
@@ -0,0 +1,39 @@
+(function($){
+    $(function() {
+        var validators = {
+            name: /^[A-Za-zА-Яа-я\s]+$/,
+            email: /^\w+\-?\.?\w+?@\w+\.\w+$/,
+            phone: /^\+7\(\d{3}\)\d{3}\-\d{4}$/,
+            default: /^[A-Za-zА-Яа-я\s]/ig
+        }
+        
+        var fields = $("input");
+        fields.on('keydown', (event) => {
+            if(event.keyCode === 13) {
+                $(event.target).parent().parent().next().find('input').focus();
+                event.preventDefault();
+            }
+        })
+
+        $('#btn-submit').on('click', (event) => {
+            for (var i = 0; i < fields.length; i++) {
+                if (!(validators[fields[i].name].test(fields[i].value))) {
+                    $(fields[i]).addClass('invalid');
+                    event.preventDefault();
+                }
+            }  
+        })
+        
+        fields.on('change', (event) => {
+            for (var i = 0; i < fields.length; i++) {
+                if ((validators[fields[i].name].test(fields[i].value))) {
+                    $(fields[i]).removeClass('invalid');
+                }
+            }
+        })
+        
+
+        
+        
+      }); 
+})(jQuery);
